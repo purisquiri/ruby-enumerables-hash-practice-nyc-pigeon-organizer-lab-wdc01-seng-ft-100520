d5be4a6a88d6ab0a6e2bd1bd64b7281aaa25f600
Mauro Dorigo <mau.dorigo@outlook.es>
2020-09-04 21:20:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  final_results = data.each_with_object({}) do |(key, value), final_array|
+    value.each do |pigeon_colors, names|
+      names.each do |name|
+        if !final_array[name]
+        final_array[name] = {}
+        end
+        if !final_array[name][key]
+          final_array[name][key] = []
+        end
+        final_array[name][key].push(pigeon_colors.to_s)
+      end  
+    end
+  end
 end
